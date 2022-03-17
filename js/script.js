8ec5567b0a4c647eccbce2f54190989f4517278c
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-03-17 08:12:48 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+  $(Docoment).ready(function(){
     $("#development-image").click(function(){
       $("#development-image").slideDown('1500').hide('1000');
       $("#development").show('1500');
